7f67ac3379c1773219479ad15c0332d5ccf9cf4d
Luis Fernando Batels <luisfbatels@gmail.com>
2024-09-07 18:19:23 -0300

    Pgsql float and timestamp fixed
    Postgres support to floats and timestamp fixeds

--- src/source/postgres.rs
+++ src/source/postgres.rs
@@ -3,9 +3,10 @@
 use super::{Driver, Query};
 use crate::value::{FieldType, TypedValue, Value};
 use async_trait::async_trait;
-use chrono::{DateTime, FixedOffset, NaiveDate, NaiveTime};
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime};
 use log::*;
-use tokio_postgres::{types::Type, Client, Error, NoTls};
+use rust_decimal::{prelude::ToPrimitive, Decimal};
+use tokio_postgres::{types::Type, Client, NoTls};
 
 pub struct PostgresDriver {
     pub conn: Option<Client>,
@@ -65,56 +66,71 @@ impl Driver for PostgresDriver {
             columns.push((col, idx, raw));
         }
 
+        let utc = FixedOffset::east_opt(0).ok_or("Invalid timezone".to_string())?;
+
         for row in qrows {
             let mut r = vec![];
 
             for (col, idx, rcol) in &columns {
-                let efmt = |e: Error| {
-                    format!(
-                        "Read column {} row {} failed: {}",
-                        col.field,
-                        r.len(),
-                        e.to_string()
-                    )
-                };
-
                 let inner = match col.kind {
                     FieldType::Integer => match rcol.type_() {
-                        &Type::INT2 => Ok(row
+                        &Type::INT2 => row
                             .try_get::<usize, Option<i16>>(*idx)
-                            .map_err(efmt)?
-                            .map(|v| TypedValue::Integer(v.into()))),
-                        &Type::INT4 => Ok(row
+                            .map_err(|e| e.to_string())
+                            .map(|ov| ov.map(|v| v.into()).map(TypedValue::Integer)),
+                        &Type::INT4 => row
                             .try_get::<usize, Option<i32>>(*idx)
-                            .map_err(efmt)?
-                            .map(|v| TypedValue::Integer(v.into()))),
-                        &Type::INT8 => Ok(row
+                            .map_err(|e| e.to_string())
+                            .map(|ov| ov.map(|v| v.into()).map(TypedValue::Integer)),
+                        &Type::INT8 => row
                             .try_get::<usize, Option<i64>>(*idx)
-                            .map_err(efmt)?
-                            .map(|v| TypedValue::Integer(v.into()))),
+                            .map_err(|e| e.to_string())
+                            .map(|v| v.map(TypedValue::Integer)),
                         _ => Err(format!("Invalid integer type {}", rcol.type_())),
-                    }?,
+                    },
                     FieldType::String => row
                         .try_get::<usize, Option<String>>(*idx)
-                        .map_err(efmt)?
-                        .map(|v| TypedValue::String(v)),
-                    FieldType::Float => row
-                        .try_get::<usize, Option<f64>>(*idx)
-                        .map_err(efmt)?
-                        .map(|v| TypedValue::Float(v)),
+                        .map_err(|e| e.to_string())
+                        .map(|v| v.map(TypedValue::String)),
+                    FieldType::Float => match rcol.type_() {
+                        &Type::FLOAT4 => row
+                            .try_get::<usize, Option<f32>>(*idx)
+                            .map_err(|e| e.to_string())
+                            .map(|ov| ov.map(|v| v.into()).map(TypedValue::Float)),
+                        &Type::FLOAT8 => row
+                            .try_get::<usize, Option<f64>>(*idx)
+                            .map_err(|e| e.to_string())
+                            .map(|ov| ov.map(|v| v.into()).map(TypedValue::Float)),
+                        &Type::NUMERIC => row
+                            .try_get::<usize, Option<Decimal>>(*idx)
+                            .map_err(|e| e.to_string())
+                            .map(|ov| ov.map(|v| v.to_f64().unwrap_or(0.0)).map(TypedValue::Float)),
+                        _ => Err(format!("Invalid float type {}", rcol.type_())),
+                    },
                     FieldType::Date => row
                         .try_get::<usize, Option<NaiveDate>>(*idx)
-                        .map_err(efmt)?
-                        .map(|v| TypedValue::Date(v)),
+                        .map_err(|e| e.to_string())
+                        .map(|v| v.map(TypedValue::Date)),
                     FieldType::Time => row
                         .try_get::<usize, Option<NaiveTime>>(*idx)
-                        .map_err(efmt)?
-                        .map(|v| TypedValue::Time(v)),
-                    FieldType::DateTime => row
-                        .try_get::<usize, Option<DateTime<FixedOffset>>>(*idx)
-                        .map_err(efmt)?
-                        .map(|v| TypedValue::DateTime(v)),
-                };
+                        .map_err(|e| e.to_string())
+                        .map(|v| v.map(TypedValue::Time)),
+                    FieldType::DateTime => match rcol.type_() {
+                        &Type::TIMESTAMPTZ => row
+                            .try_get::<usize, Option<DateTime<FixedOffset>>>(*idx)
+                            .map_err(|e| e.to_string())
+                            .map(|v| v.map(TypedValue::DateTime)),
+                        &Type::TIMESTAMP => row
+                            .try_get::<usize, Option<NaiveDateTime>>(*idx)
+                            .map_err(|e| e.to_string())
+                            .map(|ov| {
+                                ov.map(|v| DateTime::from_naive_utc_and_offset(v, utc))
+                                    .map(TypedValue::DateTime)
+                            }),
+                        _ => Err(format!("Invalid datetime type {}", rcol.type_())),
+                    },
+                }
+                .map_err(|e| format!("Column {} row {} error: {}", col.field, r.len(), e))?;
 
                 r.push(Value {
                     inner,
@@ -337,6 +353,150 @@ pub mod tests {
 
         let result = driver.fetch(query.clone()).await;
         assert_eq!(Some("Column g not found".to_string()), result.err());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn timestamp_supported_types() -> Result<(), String> {
+        let mut driver = PostgresDriver::init();
+
+        driver
+            .connect("postgresql://postgres:123@localhost/lmr_tests".to_string())
+            .await?;
+
+        let conn = driver.conn.as_ref().unwrap();
+
+        conn.execute(
+            "CREATE temp TABLE test (a timestamp with time zone, b timestamp without time zone);",
+            &[],
+        )
+        .await
+        .unwrap();
+        conn.execute("INSERT INTO test VALUES (null, null);", &[])
+            .await
+            .unwrap();
+        conn.execute(
+            "INSERT INTO test VALUES ('1996-12-19T16:39:57-08:00', '1996-12-19T16:39:57+00:00');",
+            &[],
+        )
+        .await
+        .unwrap();
+
+        let query = Query {
+            title: "Test".to_string(),
+            sql: "select * from test".to_string(),
+            fields: vec![
+                Field {
+                    title: "a".to_string(),
+                    field: "a".to_string(),
+                    kind: FieldType::DateTime,
+                },
+                Field {
+                    title: "b".to_string(),
+                    field: "b".to_string(),
+                    kind: FieldType::DateTime,
+                },
+            ],
+        };
+
+        let result = driver.fetch(query.clone()).await?;
+        assert_eq!(2, result.len());
+
+        let row = &result[0];
+        assert_eq!(None, row[0].inner);
+        assert_eq!(None, row[1].inner);
+
+        let row = &result[1];
+        assert_eq!(
+            Some(TypedValue::DateTime(
+                FixedOffset::west(8 * 3600)
+                    .with_ymd_and_hms(1996, 12, 19, 16, 39, 57)
+                    .unwrap()
+            )),
+            row[0].inner
+        );
+        assert_eq!(
+            Some(TypedValue::DateTime(
+                FixedOffset::west(0)
+                    .with_ymd_and_hms(1996, 12, 19, 16, 39, 57)
+                    .unwrap()
+            )),
+            row[1].inner
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn floats_supported_types() -> Result<(), String> {
+        let mut driver = PostgresDriver::init();
+
+        driver
+            .connect("postgresql://postgres:123@localhost/lmr_tests".to_string())
+            .await?;
+
+        let conn = driver.conn.as_ref().unwrap();
+
+        conn.execute(
+            "CREATE temp TABLE test (a real, b double precision, c numeric, d decimal);",
+            &[],
+        )
+        .await
+        .unwrap();
+
+        conn.execute("INSERT INTO test VALUES (null, null, null, null);", &[])
+            .await
+            .unwrap();
+
+        conn.execute(
+            "INSERT INTO test VALUES (123.45, 6789.01, 98765.4321, 12345.6789);",
+            &[],
+        )
+        .await
+        .unwrap();
+
+        let query = Query {
+            title: "Test".to_string(),
+            sql: "select * from test".to_string(),
+            fields: vec![
+                Field {
+                    title: "a".to_string(),
+                    field: "a".to_string(),
+                    kind: FieldType::Float,
+                },
+                Field {
+                    title: "b".to_string(),
+                    field: "b".to_string(),
+                    kind: FieldType::Float,
+                },
+                Field {
+                    title: "c".to_string(),
+                    field: "c".to_string(),
+                    kind: FieldType::Float,
+                },
+                Field {
+                    title: "d".to_string(),
+                    field: "d".to_string(),
+                    kind: FieldType::Float,
+                },
+            ],
+        };
+
+        let result = driver.fetch(query.clone()).await?;
+        assert_eq!(2, result.len());
+
+        let row = &result[0];
+        assert_eq!(None, row[0].inner);
+        assert_eq!(None, row[1].inner);
+        assert_eq!(None, row[2].inner);
+        assert_eq!(None, row[3].inner);
+
+        let row = &result[1];
+        assert_eq!(Some(TypedValue::Float(123.44999694824219)), row[0].inner);
+        assert_eq!(Some(TypedValue::Float(6789.01)), row[1].inner);
+        assert_eq!(Some(TypedValue::Float(98765.4321)), row[2].inner);
+        assert_eq!(Some(TypedValue::Float(12345.6789)), row[3].inner);
 
         Ok(())
     }
